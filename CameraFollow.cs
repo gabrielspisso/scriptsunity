5a3e8fc00cc77ee4b656725e49404f24e19e72db
gabrielspisso <gabrielhspisso@gmail.com>
2020-06-13 22:57:15 -0300

    Update CameraFollow.cs

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -12,23 +12,26 @@ public class CameraFollow : MonoBehaviour
     [Range(0,100)]
     public float radio = 100f;
 
+    private Vector3 mousePosition()
+    {
+        return new Vector3(Input.mousePosition.x,0,Input.mousePosition.y)* 0.025f;
+    }
+
     private void Start()
     {
-        Vector3 mouseOffsetInicial = new Vector3(Input.mousePosition.x,0,Input.mousePosition.y)* 0.025f;
+        Vector3 mouseOffsetInicial = mousePosition();
         mouseOffset = mouseOffsetInicial;
         offset =  transform.position - target.position - mouseOffsetInicial;
         print(offset);
     }
 
-    private float normaTurbia()
-    {
-        return (Input.mousePosition.x*Input.mousePosition.x + Input.mousePosition.y*Input.mousePosition.y) * 0.025f* 0.025f;
-    }
-
+    
     private void LateUpdate()
     {
-        if(normaTurbia() < radio*radio)
-            mouseOffset = new Vector3(Input.mousePosition.x,0,Input.mousePosition.y)* 0.025f;
+        if(mousePosition().magnitude < radio*radio)
+            mouseOffset = mousePosition();
+        else
+            mouseOffset = mousePosition().normalized * radio;
         transform.position = target.position + offset + mouseOffset;
     }
 
