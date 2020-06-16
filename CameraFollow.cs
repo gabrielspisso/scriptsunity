911a894d0468a25a0f439d645b1ab55986ada25c
gabrielspisso <gabrielhspisso@gmail.com>
2020-06-15 21:19:26 -0300

    Se cambian los nombres de los metodos, pendiente mover el comportamiento del mouse a una clase aparte

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -9,33 +9,39 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
 
     public Vector3 mouseOffset;
-    [Range(0,100)]
-    public float radio = 100f;
+    [Range(0, 3)]
+    public float radio = 3f;
 
     private Vector3 calculateMousePosition()
     {
-        return new Vector3(Input.mousePosition.x - Screen.width/2,0,Input.mousePosition.y - Screen.height/2)* 0.025f;
+        return new Vector3(Input.mousePosition.x - Screen.width / 2, 0, Input.mousePosition.y - Screen.height / 2) * 0.050f;
     }
 
     private void Start()
     {
-        Input.mousePosition.Set(Screen.width/2,Screen.height/2,0);
+        Input.mousePosition.Set(Screen.width / 2, Screen.height / 2, 0);
         Vector3 mouseOffsetInicial = calculateMousePosition();
         mouseOffset = mouseOffsetInicial;
-        offset =  transform.position - target.position;
+        offset = transform.position - target.position;
         print(offset);
     }
 
-    
+    [Range(0, 60)]
+    public int mouseMovement = 5;
+    private int framesCounter = 0;
+
     private void LateUpdate()
     {
         Vector3 mousePosition = calculateMousePosition();
-        if(mousePosition.magnitude < radio*radio)
+
+        if (mousePosition.magnitude < radio * radio)
             mouseOffset = mousePosition;
         else
             mouseOffset = mousePosition.normalized * radio * radio;
+        framesCounter = 0;
+
         transform.position = target.position + offset + mouseOffset;
-        print(mousePosition);
+        print(transform.position);
     }
 
     private void Update()
--- CameraFollowRectangle.cs
+++ CameraFollowRectangle.cs
@@ -6,7 +6,7 @@ public class CameraFollowRectangle : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
-    private  Vector3 offset;
+    private Vector3 offset;
     [SerializeField, Range(0, 100)]
     public float widthPercent = 100f;
     [Range(0, 100)]
@@ -16,15 +16,14 @@ public class CameraFollowRectangle : MonoBehaviour
 
     private float heightLimit;
 
-    private void calculateLimits()
+    //Calcula el largo y ancho del rectangulo segun los porcentajes de pantalla ingresados por el usuario
+    private void SizeRectangleArea()
     {
         widthLimit = Screen.width / 2 * widthPercent / 100f;
-        print(widthLimit);
         heightLimit = Screen.height / 2 * heightPercent / 100f;
-        print(heightLimit);
     }
 
-    private float GetXPositionValueLimited()
+    private float GetMouseX()
     {
         float xPosition = Input.mousePosition.x - Screen.width / 2;
         if (Mathf.Abs(xPosition) < widthLimit)
@@ -33,7 +32,7 @@ public class CameraFollowRectangle : MonoBehaviour
             return Mathf.Sign(xPosition) * widthLimit;
     }
 
-    private float GetYPositionValueLimited()
+    private float GetMouseY()
     {
         float yPosition = Input.mousePosition.y - Screen.height / 2;
         if (Mathf.Abs(yPosition) < heightLimit)
@@ -42,24 +41,30 @@ public class CameraFollowRectangle : MonoBehaviour
             return Mathf.Sign(yPosition) * heightLimit;
     }
 
-    private Vector2 GetInputValues()
+    private Vector2 GetMousePosition()
     {
-        calculateLimits();
-        float x = GetXPositionValueLimited();
-        float y = GetYPositionValueLimited();
+        SizeRectangleArea();
+        float x = GetMouseX();
+        float y = GetMouseX();
         return new Vector2(x, y);
+    }
+
+    private Vector3 Get3DMousePosition(float scaleFactor)
+    {
+        Vector2 mousePosition = GetMousePosition();
+        return new Vector3(mousePosition.x, 0, mousePosition.y) * scaleFactor;
     }
 
     private void Start()
     {
-        Input.mousePosition.Set(Screen.width / 2, Screen.height / 2, 0);
+        SizeRectangleArea();
+        Input.mousePosition.Set(Screen.width / 2, Screen.height / 2, 0);//Mueve el puntero al medio de la pantalla
         offset = transform.position - target.position;
     }
 
     private void LateUpdate()
     {
-        Vector2 mousePosition = GetInputValues();
-        Vector3 mouseOffset = new Vector3(mousePosition.x, 0, mousePosition.y);
-        transform.position = target.position + offset + mouseOffset * 0.025f;
+        Vector3 mouseOffset = Get3DMousePosition(0.025f);
+        transform.position = target.position + offset + mouseOffset;
     }
 }
